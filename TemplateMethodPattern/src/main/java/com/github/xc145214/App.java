1b4369f282577ff581883a944c9805e64c075c45
xc145214 <61106254@qq.com>
2018-05-28 23:18:37 +0800

    模板方法模式

---
+++ TemplateMethodPattern/src/main/java/com/github/xc145214/App.java
@@ -0,0 +1,20 @@
+package com.github.xc145214;
+
+import com.github.xc145214.templateMethod.HummerH1Model;
+import com.github.xc145214.templateMethod.HummerH2Model;
+import com.github.xc145214.templateMethod.HummerModel;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        //客户开着H1型号，出去遛弯了
+        HummerModel h1 = new HummerH1Model();
+        h1.run(); //汽车跑起来了；
+
+        //客户开H2型号，出去玩耍了
+        HummerModel h2 = new HummerH2Model();
+        h2.run();
+    }
+}
