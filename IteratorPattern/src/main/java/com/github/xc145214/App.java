6ab102fa9826a23b9ffe689d69f331ee7341474a
xc145214 <61106254@qq.com>
2018-06-07 22:43:17 +0800

    迭代器模式基本

---
+++ IteratorPattern/src/main/java/com/github/xc145214/App.java
@@ -0,0 +1,10 @@
+package com.github.xc145214;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ IteratorPattern/src/main/java/com/github/xc145214/iterator/IProject.java
@@ -0,0 +1,11 @@
+package com.github.xc145214.iterator;
+
+/**
+ * @author xiac xc145214@gmail.com
+ * @date 2018/6/7 0007 22:39
+ */
+public interface IProject {
+
+    //从老板这里看到的就是项目信息
+    public String getProjectInfo();
+}
